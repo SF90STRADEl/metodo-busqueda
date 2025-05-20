7f0e988cfb0c678cedf5fe15266f8cd7aede28c8
miguel cortes <cvd4kang@gmailcom>
2025-05-19 20:49:33 -0600

    Agregar archivos de proyecto.

---
+++ metodo busqueda/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Métodos_de_busqueda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            metodos metodos = new metodos();
+
+
+            int opt;
+
+            int[] array = new int[] { 1, 4, 6, 8, 10, 13, 16, 30, 136, 346, 578, 9735 };
+
+            hash hash = new hash(array.Length);
+
+            while (true)
+            {
+                Console.WriteLine("\nArreglo:");
+                metodos.print(array);
+
+                Console.WriteLine("\n=== Menú de Búsqueda y Hashtable ===");
+                Console.WriteLine("1. Búsqueda Secuencial");
+                Console.WriteLine("2. Búsqueda Binaria");
+                Console.WriteLine("3. Agregar Arreglo A Hashtable");
+                Console.WriteLine("4. Mostrar Hashtable");
+                Console.WriteLine("5. Buscar en Hashtable");
+                Console.WriteLine("6. Salir");
+                Console.Write("Seleccione una opción: ");
+                opt = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+
+
+                switch (opt)
+                {
+                    case 1:
+                        Console.Write("Inserte el valor a buscar: ");
+                        int valSeq = Convert.ToInt32(Console.ReadLine());
+                        metodos.Secuential_unordered(array, valSeq);
+                        break;
+
+                    case 2:
+                        Console.Write("Inserte el valor a buscar: ");
+                        int valBin = Convert.ToInt32(Console.ReadLine());
+                        metodos.Binary(array, valBin);
+                        break;
+
+                    case 3:
+                        int clave = 0;
+                        foreach (var valor in array)
+                        {
+                            hash.Insert(clave, valor);
+                            clave++;
+                        }
+                        break;
+
+                    case 4:
+                        hash.DisplayTable();
+                        break;
+
+                    case 5:
+                        Console.Write("Inserte la clave a buscar: ");
+                        int keySearch = Convert.ToInt32(Console.ReadLine());
+                        hash.Get(keySearch);
+                        break;
+                    case 6:
+                        Console.WriteLine("Gracias por usar el programa");
+                        return;
+
+                    default:
+                        Console.WriteLine("Opción no válida.");
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ metodo busqueda/metodos.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Métodos_de_busqueda
+{
+    internal class metodos
+    {
+        public void print(int[] array)
+        {
+            foreach (int k in array)
+            {
+                Console.Write(k + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Binary(int[] array, int x)
+        {
+
+
+            int left, cen, right;
+            bool ban;
+            int n = array.Length - 1;
+
+            left = 0;
+            right = n;
+            ban = false;
+            cen = 0;
+
+            while ((left <= right) && (ban == false))
+            {
+                cen = (left + right) / 2;
+
+                if (array[cen] == x)
+                {
+                    ban = true;
+                }
+                else
+                {
+                    if (x > array[cen])
+                    {
+                        left = cen + 1;
+                    }
+                    else
+                    {
+                        right = cen - 1;
+                    }
+                }
+
+            }
+            print(array);
+            if (ban == true)
+            {
+                Console.WriteLine("The information is in the position: " + cen);
+            }
+            else
+            {
+                Console.WriteLine("The information is not in the array");
+            }
+        }
+
+        public void Secuential_unordered(int[] v, int x)
+        {
+            int i = 0;
+            int n = v.Length - 1;
+
+            while ((i <= n) && (v[i] != x))
+            {
+                i++;
+            }
+            if (i > n)
+            {
+                Console.WriteLine("The information is not in the array");
+            }
+            else
+            {
+                print(v);
+                Console.WriteLine("The information is in the position: " + i);
+            }
+        }
+    }
+}
